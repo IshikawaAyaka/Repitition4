bdfe2014891da74c9c8cca75042430e85e984022
Yamaura Hidemi <h-yamaura@cresco.co.jp>
2020-06-12 19:14:02 +0900

    fa

--- 04_実装/HardMode.java
+++ 04_実装/HardMode.java
@@ -1,11 +1,69 @@
-public class HardMode extends Mode {
+package typingGame;
 
-	public HardMode() {
 
+public class HardMode extends Mode{
+	//候補の文字列
+	String[] x = new String[36];
+	
+	//ランダムな数を生成
+	int y;
+	
+	//出力するための文字列
+	String[] z = new String[7];
+	
+	//配列を生成する
+	public HardMode(){
+		x[0] = "0";
+		x[1] = "1";
+		x[2] = "2";
+		x[3] = "3";
+		x[4] = "4";
+		x[5] = "5";
+		x[6] = "6";
+		x[7] = "7";
+		x[8] = "8";
+		x[9] = "9";
+		x[10] = "a";
+		x[11] = "b";
+		x[12] = "c";
+		x[13] = "d";
+		x[14] = "e";
+		x[15] = "f";
+		x[16] = "g";
+		x[17] = "h";
+		x[18] = "i";
+		x[19] = "j";
+		x[20] = "k";
+		x[21] = "l";
+		x[22] = "m";
+		x[23] = "n";
+		x[24] = "o";
+		x[25] = "p";
+		x[26] = "q";
+		x[27] = "r";
+		x[28] = "s";
+		x[29] = "t";
+		x[30] = "u";
+		x[31] = "v";
+		x[32] = "w";
+		x[33] = "x";
+		x[34] = "y";
+		x[35] = "z";
 	}
-
-	public void setAnswer() {
-
+	//ランダムで選んだ文字を順番に配列に入れる
+	public String[] setAnswer(){
+		for(int i=0;i<7;i++) {
+			 y = (int)((Math.random() * 35) );
+			  z[i] = x[y];
+			  }
+		return z;
+	}
+	
+	public void getAnswer() {
+		for(int i=0;i<7;i++) {
+			System.out.print(z[i]);
+		}
+		
 	}
 
 }
