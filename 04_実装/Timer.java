cb3a07cc9e853169cf1e2e5a8682e313512d2ab4
Ota Matsuri <m-ota@cresco.co.jp>
2020-06-15 09:25:39 +0900

    更新

--- 04_実装/Timer.java
+++ 04_実装/Timer.java
@@ -1,23 +1,30 @@
 package typingGame;
 
 public class Timer {
-
+	
+	//経過時間
 	private long time;
+	//開始時間
 	private long startTime;
+	//終了時間
 	private long stopTime;
-
+	
+	//コンストラクタ
 	public Timer() {
 	}
-
+	
+	//経過時間を返す
 	public long getTime() {
 		time=this.stopTime-this.startTime;
 		return time/1000000;
 	}
-
+	
+	//開始時間を格納する
 	public void timeStart() {
 		startTime = System.nanoTime();
 	}
-
+	
+	//終了時間を格納する
 	public void timeStop() {
 		stopTime = System.nanoTime();
 	}
