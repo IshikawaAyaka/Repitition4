a16509586bc89180e5299f5ec0e18a92cda6f6bd
Ota Matsuri <m-ota@cresco.co.jp>
2020-06-15 09:48:20 +0900

    a

--- 04_実装/EasyMode.java
+++ 04_実装/EasyMode.java
@@ -3,15 +3,11 @@ package typingGame;
 public class EasyMode extends Mode {
 
 	// 候補の文字の配列を生成
-	String[] x = new String[10];
+	private String[] x = new String[10];
 	
-	String str;
-
-	// ランダムな数を生成
-	int y;
-
 	// 出力するための文字列を生成
-	String[] z = new String[7];
+	private String[] z = new String[7];
+	private String str;
 
 	// 配列を生成する
 	public EasyMode() {
@@ -29,6 +25,7 @@ public class EasyMode extends Mode {
 
 	// ランダムに選んだ文字を順番に配列に入れる
 	public String setAnswer() {
+		int y;
 		for (int i = 0; i < 7; i++) {
 			y = (int) (Math.random() * 9);
 			z[i] = x[y];
